ccb719f364ca25b2498dfad34de0cffcba516948
hammadhttps <hammadulhassan2620@gmail.com>
2026-03-15 06:43:23 +0500

    add moviesCard component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,14 @@
 import HeroSection from '../components/HeroSection'
 import heroImageUrl from '../assets/heroImage.jpg'
+import CardList from '../components/CardList'
 
 const HomePage = () => {
   return (
-    <div>
+    <div className=' bg-[#F5F2F2]'>
         <HeroSection heroImage={heroImageUrl}/>
+        <CardList/>
+        <CardList/>
+        <CardList/>
     </div>
   )
 }
